8263ee0479c3aa9453f6fd93ed9ab6e708771af6
rob2fresh4this <grijrob07@gmail.com>
2025-03-17 14:25:09 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import WeatherApp from "./components/WeatherApp"
+
+
+function App() {
+  return (
+    <>
+      <WeatherApp />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/WeatherApp.tsx
@@ -0,0 +1,24 @@
+import StarIcon from '../../Assets/star-regular (1).svg'
+import MagnifyingGlass from '../../Assets/magnifying-glass-solid (1).svg'
+
+const WeatherApp = () => {
+    return (
+        <>
+            <div>
+                <div>
+                    <div className='text-[32px]'>Stockton, CA</div>
+                    <img className="w-[41px]" src={StarIcon} alt="Star" />
+                </div>
+                <div>
+                    <div className='flex items-center'>
+                        <img className='w-[30px]' src={MagnifyingGlass} alt="Magnifying Glass" />
+                        <input className='text-[18px]' type="text" placeholder="Search Location" />
+                    </div>
+                    <img className="w-[41px]" src={StarIcon} alt="Favorites" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default WeatherApp
